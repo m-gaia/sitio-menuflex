a6697ac32c1155652686cd413dc68e0e719242d6
Marianela <gaia.marianela@gmail.com>
2021-05-24 13:44:05 -0300

    terminado

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,7 +2,7 @@ addEventListener('DOMContentLoaded', () => {
     const btn_menu = document.querySelector('.btn-menu')
     if (btn_menu) {
         btn_menu.addEventListener('click', () => {
-            const menu_items = document.querySelector('menu-items')
+            const menu_items = document.querySelector('.menu-items')
             menu_items.classList.toggle('show')
         })
     }
